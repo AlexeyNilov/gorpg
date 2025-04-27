335f2e681e3dd35115747d7c8dd26ba8ee76cf96
Alexey Nilov <alexeyn00000@gmail.com>
2025-04-27 17:06:45 +0200

    add eater

---
+++ simulation/consumer/eater.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/AlexeyNilov/gorpg/ooda"
+	"github.com/AlexeyNilov/gorpg/stuff/transformer"
+)
+
+type Eater struct {
+	transformer.Transformer
+	PointOfView string
+}
+
+func (e *Eater) Observe() string {
+	return e.PointOfView
+}
+
+func (e *Eater) Orient(observation string) string {
+	if observation == "Day" {
+		return "Time to eat"
+	}
+	return "Time to sleep"
+}
+
+func (e *Eater) Decide(orientation string) string {
+	if orientation == "Time to eat" {
+		return "Eat"
+	}
+	return "Sleep"
+}
+
+func (e *Eater) Act(decision string) string {
+	if decision == "Eat" {
+		return "Consume"
+	}
+	return ""
+}
+
+func (e *Eater) Execute(process ooda.OODAProcess) {
+	process.Run(e, e, e, e)
+	if process.Action == "Consume" {
+		e.Transformer.Consume()
+	}
+}
+
+func main() {
+	process := ooda.OODAProcess{}
+	eater := Eater{Transformer: transformer.Transformer{}}
+	eater.PointOfView = "Day"
+	eater.Execute(process)
+	eater.Execute(process)
+	eater.PointOfView = "Night"
+	eater.Execute(process)
+	fmt.Print(eater, "\n")
+}
