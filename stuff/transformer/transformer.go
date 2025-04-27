8ba766e90464cc8e0eee1ff3dcb7a641b7cd126b
Alexey Nilov <alexeyn00000@gmail.com>
2025-04-27 23:06:35 +0200

    refactoring

--- stuff/transformer/transformer.go
+++ stuff/transformer/transformer.go
@@ -3,30 +3,29 @@ package transformer
 import "github.com/AlexeyNilov/gorpg/stuff/resource"
 
 type Transformer struct {
-	Consumed int
-	Produced int
+	ConsumedCount int
+	ProducedCount int
 }
 
 func (t *Transformer) Consume() {
-	t.Consumed++
+	t.ConsumedCount++
 }
 
 func (t *Transformer) Produce() {
-	t.Produced++
+	t.ProducedCount++
 }
 
 type ResourceTransformer struct {
-	ConsumedCount  int
-	Capacity int
-	ProducedCount  int
+	Transformer
+	resource.Resource
 }
 
 func (t *ResourceTransformer) Consume(getter resource.Getter, amount int) {
-	t.Capacity += getter.Get(amount)
+	t.Resource.Put(getter.Get(amount))
 	t.ConsumedCount++
 }
 
 func (t *ResourceTransformer) Produce(putter resource.Putter, amount int) {
-	t.Capacity -= putter.Put(amount)
+	putter.Put(t.Resource.Get(amount))
 	t.ProducedCount++
 }
--- stuff/transformer/transformer_test.go
+++ stuff/transformer/transformer_test.go
@@ -10,27 +10,56 @@ import (
 func TestConsume(t *testing.T) {
 	testTransformer := Transformer{}
 	testTransformer.Consume()
-	assert.Equal(t, 1, testTransformer.Consumed)
+	assert.Equal(t, 1, testTransformer.ConsumedCount)
 }
 
 func TestProduce(t *testing.T) {
 	testTransformer := Transformer{}
 	testTransformer.Produce()
-	assert.Equal(t, 1, testTransformer.Produced)
+	assert.Equal(t, 1, testTransformer.ProducedCount)
 }
 
-func TestPut(t *testing.T) {
+func TestResourceConsume(t *testing.T) {
+	// Initialize resources and transformer
 	testResource := resource.Resource{Value: 5, MaxValue: 20}
-	testResourceTransformer := ResourceTransformer{}
-	testResourceTransformer.Consume(&testResource, 5)
+	testTransformer := ResourceTransformer{
+		Transformer: Transformer{},
+		Resource:    resource.Resource{Value: 0, MaxValue: 10},
+	}
 
-	assert.Equal(t, 0, testResource.Value)
-	assert.Equal(t, 1, testResourceTransformer.ConsumedCount)
-	assert.Equal(t, 5, testResourceTransformer.Capacity)
+	// Test consuming resources
+	testTransformer.Consume(&testResource, 5)
+	assert.Equal(t, 0, testResource.Value, "Resource value should be depleted after consuming 5")
+	assert.Equal(t, 1, testTransformer.ConsumedCount, "Transformer should record one consumption")
+	assert.Equal(t, 5, testTransformer.Value, "Transformer value should increase by consumed amount")
 
-	testResourceTransformer.Produce(&testResource, 5)
+	// Test buffer overflow during consumption
+	testResource = resource.Resource{Value: 15, MaxValue: 20}
+	testTransformer.Value = 0
+	testTransformer.Consume(&testResource, 15)
+	assert.Equal(t, 10, testTransformer.Value, "Transformer should cap value at max capacity (10)")
+	assert.Equal(t, 0, testResource.Value, "Excess resource should be wasted")
 
-	assert.Equal(t, 5, testResource.Value)
-	assert.Equal(t, 1, testResourceTransformer.ProducedCount)
-	assert.Equal(t, 0, testResourceTransformer.Capacity)
+}
+
+func TestResourceProduce(t *testing.T) {
+	// Initialize resources and transformer
+	testResource := resource.Resource{Value: 0, MaxValue: 20}
+	testTransformer := ResourceTransformer{
+		Transformer: Transformer{},
+		Resource:    resource.Resource{Value: 5, MaxValue: 10},
+	}
+
+	// Test producing resources
+	testTransformer.Produce(&testResource, 5)
+	assert.Equal(t, 5, testResource.Value, "Resource value should increase by 5 after production")
+	assert.Equal(t, 1, testTransformer.ProducedCount, "Transformer should record one production")
+	assert.Equal(t, 0, testTransformer.Value, "Transformer value should decrease after production")
+
+	// Test buffer overflow during production
+	testResource = resource.Resource{Value: 0, MaxValue: 20}
+	testTransformer.Value = 10
+	testTransformer.Produce(&testResource, 15)
+	assert.Equal(t, 0, testTransformer.Value, "Transformer value should be depleted after production")
+	assert.Equal(t, 10, testResource.Value, "Resource value should cap at max Transformer capacity (10)")
 }
