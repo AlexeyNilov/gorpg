e76bfd288d1fcd92088f390bc5d6148b1416f263
Alexey Nilov <alexeyn00000@gmail.com>
2025-04-30 17:30:44 +0200

    making space

---
+++ demo/space/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/AlexeyNilov/gorpg/system/space"
+)
+
+func main() {
+	space := space.CreateSpace(10)
+	for _, point := range *space {
+		fmt.Print(point.ID, "-")
+	}
+
+	fmt.Println()
+	for _, point := range *space {
+		fmt.Print(point.Next.Next.ID, "-")
+	}
+}
